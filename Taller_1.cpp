e538b838e7f4ff79597f3ef20e1cf492f41f217c
Gabrri <55514542+Gabrri@users.noreply.github.com>
2020-01-23 08:51:51 -0500

    Update Taller_1.cpp

--- Taller_1.cpp
+++ Taller_1.cpp
@@ -86,4 +86,113 @@ using namespace std;
   ///////////////////////////////////////////////////////////////////////////
 // 2
 
-// 
+
+/*int main (){
+  vector<int> myvector6;
+// primeras potencias de 3
+
+  for(int i = 0; i < 11; i++)
+    myvector6.push_back(pow(3,i));
+
+  for(int i = 0; i < myvector6.size(); i++)
+    //cout << myvector6[i] << endl;
+// eliminar las potencias menores a 100
+
+  for(int i = 0; i < myvector6.size(); i++)
+    if(myvector6[i] > 101)
+      myvector6.erase(myvector6.begin()+i);
+
+  for(int i = 0; i < myvector6.size(); i++)
+    cout << myvector6[i] << endl;
+
+
+  return 0;
+
+}*/
+////////////////////////////////////////////////////////////////////////////////s
+
+// 3 letras del alfabeto
+
+/*int main (){
+  vector<char> myvector;
+  char a;
+
+  for(a = 'a'; a <= 'z'; a++)
+    myvector.push_back(a);
+
+  for(int i = 0; i < myvector.size(); i++)
+    cout << myvector[i] << endl;
+
+  return 0;
+}*/
+
+// lea una lista de frutas de cin y guardarla en el vector
+
+int main(){
+  vector<string> listafrutas;
+  string fruta;
+  cout << "Ingrese la fruta: " << endl;
+  cin >> fruta;
+  listafrutas.push_back(fruta);
+
+  cout << "La lista de frutas contiene: " ;
+  for(int i = 0; i < listafrutas.size(); i++)
+    cout << listafrutas[i] << endl;
+
+  return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+// 4
+
+int main(){
+
+  list<string> lista = {"casa1", "casa2", "casa3", "casa4"};
+  vector<string> iden(lista.begin(), lista.end());
+
+  list<int> lista2 ={1,2,3,4};
+  vector<int> dist(lista2.begin(),lista2.end());
+
+  for(int i = 0; i < iden.size(); i++)
+    cout << iden[i] << "--->" << dist[i] << endl;
+
+
+  return 0;
+}
+////////////////////////////////////////////////////////////////////////////////7
+
+// 1 tamaño de una lista
+
+int main (){
+  list<int> mylist;
+  mylist.push_back(1);
+  mylist.push_back(2);
+  mylist.push_back(3);
+  mylist.push_back(4);
+  mylist.push_back(5);
+
+  cout << "Size of my list: " << mylist.size() << endl;
+
+  if(!mylist.empty())
+    cout << "mi lista no esta vacia" << endl;
+    else cout << "mi lista esta vacia" << endl;
+
+  cout << "primer elemento de mi lista: " << mylist.front() << endl;
+  cout << "ultimo elemento de mi lista: " << mylist.back() << endl;
+
+  mylist.pop_front();
+  mylist.pop_back();
+
+  cout << "primer elemento de mi lista: " << mylist.front() << endl;
+  cout << "ultimo elemento de mi lista: " << mylist.back() << endl;
+
+
+
+  return 0;
+
+}
+
+
+
+
