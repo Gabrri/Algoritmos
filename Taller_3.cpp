ab5199f89e80b0983ce07653ba28db4c524d7d56
Gabrri <55514542+Gabrri@users.noreply.github.com>
2020-02-06 08:46:28 -0500

    Terminar 3 y 4

--- Taller_3.cpp
+++ Taller_3.cpp
@@ -1,28 +1,54 @@
-#include <list>
+#include <vector>
 #include <iostream>
 using namespace std;
 
 // 1
-int main (){
-  list<int> mylist;
-  list<int>::iterator it;
+// int main (){
+//   vector<int> mylist;
+//   vector<int>::iterator it;
+//
+//   mylist.push_back(1);
+//   mylist.push_back(2);
+//   mylist.push_back(3);
+//   mylist.push_back(5);
+//   mylist.push_back(6);
+//   mylist.push_back(7);
+//
+//   for(unsigned int i = 1; i <= mylist.size() ;i++){
+//     int m = mylist[i+1];
+//     if(m != mylist[i+1]){
+//       it = mylist.begin();
+//       mylist.insert(it+i-1,i+1);
+//     }
+//   }
+//
+//   for(unsigned int i = 1 ;i <= mylist.size();i++)
+//     cout << mylist[i] << endl;
+//
+//   return 0;
+// }
+////////////////////////////////////////////////////////////////////////////////
 
-  mylist.push_back(1);
-  mylist.push_back(2);
-  mylist.push_back(3);
-  mylist.push_back(5);
-  mylist.push_back(6);
-  mylist.push_back(7);
+// 2
+//Encontrar e iprimir el elemento mas chico
 
-  for(it = mylist.begin();it != mylist.end();it++){
-    int m = *it;
-    if(m+1 != *it+1){
-      mylist.insert(it,*it);
+int main(){
+  vector<string> myvector;
+  string elem;
+
+  myvector.push_back("hola");
+  myvector.push_back("ola");
+  myvector.push_back("lola");
+  myvector.push_back("bola");
+
+  for(unsigned int i = 0; i <= myvector.size(); i++){
+    if(myvector[i] < myvector[i+1]){
+      elem = myvector[i];
     }
   }
 
-  for(it = mylist.begin();it != mylist.end();it++)
-    cout << *it << endl;
+  cout <<elem << " el mas peque" << endl;
+
 
   return 0;
 }
