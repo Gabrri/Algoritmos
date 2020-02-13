7538e73d67e04c1729bb999bbcc73c0935af50ee
Gabrri <55514542+Gabrri@users.noreply.github.com>
2020-02-13 08:14:29 -0500

    Update Taller_4.cpp

--- Taller_4.cpp
+++ Taller_4.cpp
@@ -4,10 +4,10 @@
 #include "functions.hpp"
 using namespace std;
 
-// 1 Bidirectional bubble sort 
+// 1 Bidirectional bubble sort
 
 
-// bubble sort normal 
+// bubble sort normal
 void  bubble_sort(vector<int>& v){
 	for(unsigned int i = 0; i < v.size() ;i++){
 		for(unsigned int j =0; j < v.size(); j++){
@@ -17,19 +17,17 @@ void  bubble_sort(vector<int>& v){
 	}
 }
 
-// ordenar en dos direcciones Bidirectinal bubble sort 
+// ordenar en dos direcciones Bidirectinal bubble sort
 // elemento mas chico de primeras elemento mas grande de ultimas
-// 2 elemento chas chico de segundas, elemento menos grande de penultimas 
+// 2 elemento chas chico de segundas, elemento menos grande de penultimas
 // abajo hacia arriba, pasa alternativamente de abajo hacia arriba y luego de arriba hacia abajo
-// 
-
-void bidirectional_bs(vector <int>& vec){
+void bidirectional_bubble_sort(vector<int>& vec){
+	
 }
-
 
 int main(){
   vector<int> vec;
-  
+
   vec.push_back(7);
   vec.push_back(6);
   vec.push_back(5);
@@ -37,20 +35,55 @@ int main(){
   vec.push_back(2);
   vec.push_back(1);
   vec.push_back(3);
-  
+
   for(unsigned int i = 1; i <= vec.size(); i++)
 		cout << "Sin hacer bubble sort " << vec[i] << " " <<  endl;
 
   bubble_sort(vec);
-  
+
 	for(unsigned int i = 1; i <= vec.size(); i++)
 		cout << "haciendo bubble sort " << vec[i] <<  endl;
-  
+
   return 0;
 }
 //////////////////////////////////////////////////////////////////////////////////////////
 
 // 2
+// selection sort
 
+void selection_sort(vector<int>& vec){
+	for(unsigned int i = 0; i < vec.size(); i++){
+		int minIndex = i;
+		for(unsigned int j = i+1; j < vec.size();j++){
+			if(vec[j] < vec[minIndex]){
+				minIndex = j;
+			}
+		}
+		swap(vec[i], vec[minIndex]);
+	}
+}
 
+void double_selection_soret(vector<int>& vec){
 
+}
+int main(){
+	vector<int> vec;
+
+	vec.push_back(10);
+	vec.push_back(9);
+	vec.push_back(1);
+	vec.push_back(3);
+	vec.push_back(5);
+	vec.push_back(6);
+	vec.push_back(7);
+	vec.push_back(2);
+	vec.push_back(4);
+	vec.push_back(8);
+
+	selection_sort(vec);
+
+	for(unsigned int i =0; i < vec.size(); i++)
+		cout << vec[i] << endl;
+
+	return 0;
+}
