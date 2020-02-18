5d857a45b1165c08401e3fdb021673d463189752
Gabrri <55514542+Gabrri@users.noreply.github.com>
2020-02-18 08:26:27 -0500

    revisar

--- Code_Class_Busqueda/binary_search.cpp
+++ Code_Class_Busqueda/binary_search.cpp
@@ -5,9 +5,20 @@ using namespace std;
 
 
 int binary_search(int X, const vector<int>& v){
-  .
-  .
-  .
+  int left = 0, right = v.size()-1;
+  while(left <= right){
+    int mid = (right+left) / 2;
+    if(v[mid] == X)
+      return mid;
+    else{
+      if(X > v[mid])
+        left = mid+1;
+      else
+        right = mid-1;
+
+      }
+    }
+    return -1;
 }
 
 int main(){
@@ -19,26 +30,24 @@ int main(){
   //random number generator
   unsigned seed = 123;
   minstd_rand0 rng(seed);
+  generate_vector(int size, vector<int>& vec, minstd_rand0& rng)
 
   double tot_time = 0;
   for(int n = 0; n < times; ++n){
     //Generate the random vector
-    .
-    .
-    
     //print vector
-    //print_vector(vec);
-    
+    print_vector(vec);
+
     //get initial time
     double tstart = gettime();
-    
+
     //Search for X in vec
     int pos = binary_search(X, vec);
-    
+
     //final time
     double tstop = gettime();
     tot_time += tstop-tstart;
-    
+
     if(pos < 0)
       cout << X << " is not contained in the vector\n";
     else
@@ -46,6 +55,6 @@ int main(){
 
   }
   cout << "Time taken = " << tot_time / times << endl;
-  
+
   return 0;
 }
