113911e55e4ddde1a101b05a519345bcb6ce9dce
Gabrri <55514542+Gabrri@users.noreply.github.com>
2020-02-18 08:16:30 -0500

    check

--- Code_Class_Busqueda/linear_search.cpp
+++ Code_Class_Busqueda/linear_search.cpp
@@ -5,45 +5,51 @@ using namespace std;
 
 
 int linear_search(int X, const vector<int>& v){
-  .
-  .
-  .
+  for(unsigned int i = 0; i < v.size(); ++i){
+    if(v[i] == X)
+      return i;
+  }
+  return -1;
 }
 
 int main(){
-  int times = 100;
-  int size = 1e6;
-  int X = 1;//size-1;
-  vector<int> vec;
-  
-  //random number generator
-  unsigned seed = 123;
-  minstd_rand0 rng(seed);
+  // int times = 100;
+  // //int size = 1e6;
+  // int X = 1;//size-1;
+  // vector<int> vec;
+  //
+  // //random number generator
+  // unsigned seed = 123;
+  // minstd_rand0 rng(seed);
+  //
+  // double tot_time = 0;
+  // for(int n = 0; n < times; ++n){
+  //   //Generate the random vector
+  //
+  //   //get initial time
+  //   double tstart = gettime();
 
-  double tot_time = 0;
-  for(int n = 0; n < times; ++n){
-    //Generate the random vector
-    .
-    .
-    
-    
-    //get initial time
-    double tstart = gettime();
-    
     //Search for X in vec
+    vector<int> vec;
+
+    for(int i =1; i < 122; i++)
+      vec.push_back(i);
+
+    int X = 47;
+
     int pos = linear_search(X, vec);
-    
+
     //final time
-    double tstop = gettime();
-    tot_time += tstop-tstart;
-    
+    // double tstop = gettime();
+    // tot_time += tstop-tstart;
+
     if(pos < 0)
       cout << X << " is not contained in the vector\n";
     else
       cout << X << " was found at position " << pos << "\n";
 
-  }
-  cout << "Time taken = " << tot_time / times << endl;
-  
+  // }
+  // cout << "Time taken = " << tot_time / times << endl;
+
   return 0;
 }
