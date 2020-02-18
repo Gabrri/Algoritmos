0fa0771f6e62412ed77d7de1b2940df71003fda9
Gabrri <55514542+Gabrri@users.noreply.github.com>
2020-02-18 08:41:10 -0500

    revisar

--- Code_Class_Busqueda/selection_sort.cpp
+++ Code_Class_Busqueda/selection_sort.cpp
@@ -6,24 +6,27 @@ using namespace std;
 
 
 void selection_sort(vector<int>& v){
-  .
-  .
-  .
+  for(int i =0; i < int(v.size()-1); i++){
+    int min_id = i;
+    for(int j = i+1; j < int(v.size()); ++j){
+      if(v[j] < v[min_id])
+        min_id = j;
+    }
+    swap(i, min_id, v);
+  }
 }
 
 
 int main(){
-  int times = 100;
-  int size = 1e3;
-  vector<int> vec;
-
-  //random number generator
   unsigned seed = 123;
   minstd_rand0 rng(seed);
+  vector<int> vec;
 
-  .
-  .
-  .
-  
+  generate_vector(500, vec, rng);
+
+  selection_sort(vec);
+  print_vector(vec);
+
+
   return 0;
 }
