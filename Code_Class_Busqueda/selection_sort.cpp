1bc80c38f5ccf8b4bc6d82d168c1adbf970fdf74
Gabrri <55514542+Gabrri@users.noreply.github.com>
2020-02-20 08:22:12 -0500

    Check

--- Code_Class_Busqueda/selection_sort.cpp
+++ Code_Class_Busqueda/selection_sort.cpp
@@ -3,7 +3,11 @@
 #include "functions.hpp"
 using namespace std;
 
-
+void swap(int i, int j, vector<int>& v){
+  int temp = v[i];
+  v[i] = v[j];
+  v[j] = temp;
+}
 
 void selection_sort(vector<int>& v){
   for(int i =0; i < int(v.size()-1); i++){
@@ -16,18 +20,39 @@ void selection_sort(vector<int>& v){
   }
 }
 
+// generar un vector aleatorio
+
+void generate_vector(int size, vector<int>& v, minstd_rand0& rng){
+  v.clear();
+  const int max_num = size;
+  //Generation of a sequence of (pseudo)random numbers
+  rng();
+  for(int i = 0; i < size; ++i){
+    int num = int(max_num * ( double(rng()) / rng.max() ));
+    v.push_back(num);
+  }
+}
+
+void print_vector(vector<int>& v){
+  for(int i = 0; i < v.size(); ++i)
+    cout << v[i] << " ";
+  cout << "\n";
+}
 
 int main(){
   unsigned seed = 123;
   minstd_rand0 rng(seed);
   vector<int> vec;
 
-  generate_vector(500, vec, rng);
+  generate_vector(200, vec, rng);
+  print_vector(vec);
 
   selection_sort(vec);
   print_vector(vec);
 
 
+
+
+
   return 0;
 }
-
