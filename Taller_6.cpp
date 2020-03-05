77d099bdd6c8543cc34475a27c3abf3da527058a
Gabrri <55514542+Gabrri@users.noreply.github.com>
2020-03-04 19:14:56 -0500

    terminar taller

--- Taller_6.cpp
+++ Taller_6.cpp
@@ -16,7 +16,7 @@ using namespace std;
 //   }
 // }
 // int main (){
-//   int m =kpot(5,2);
+//   int m =kpot(8,8);
 //   cout << m << endl;
 //
 //   return 0;
@@ -25,20 +25,47 @@ using namespace std;
 
 // 2
 
-string prog(string m){
-  if(m.size()==0){
-    return m;
-  }else if (m.size() > 0){
-    string temp = m.substr(0, m.size()-1);
-    string s_temp = prog(s_temp);
-    return s_temp;
-  }else{
-    return 0;
-  }
-}
+// string prog(string m){
+//     if(m.size()==1){
+//     return m;
+//   }else if(m.size()==0){
+//     return 0;
+//   }else{
+//     for(int i =1; i <= int(m.size()+1); i++)
+//     cout << m[m.size()-i] << endl;
+//   }
+//   return 0;
+// }
+//
+// int main (){
+//   string z = prog("hola");
+//   cout << z << endl;
+//   return 0;
+// }
+////////////////////////////////////////////////////////////////
 
-int main (){
-  string z = prog("hola");
-  cout << z << endl;
-  return 0;
-}
+// 3
+
+// bool palindromo(string pal){
+//   if(pal.size()==0){
+//     return true;
+//   }else if (pal.size()==1){
+//     return true;
+//   }else if(pal[0] == pal[pal.size()-1]){
+//     string cadena = pal.substr(1, pal.size()-1);
+//     palindromo (cadena);
+//     return true;
+//   }else{
+//     return false;
+//   }
+// }
+//
+// int main(){
+//   cout << palindromo("sometamosomatemos") << endl;
+//   return 0;
+// }
+
+
+///////////////////////////////////////////////////////////////////
+
+// 4
